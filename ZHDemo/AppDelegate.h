206676f666c13cd39facf9cadcbabadbdc260077
HuZhiLo <huzhi@huzhideimac.local>
2017-07-19 15:06:21 +0800

    Initial Commit

---
+++ ZHDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHDemo
+//
+//  Created by huzhi on 2017/7/19.
+//  Copyright © 2017年 ZhiHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHDemo
+//
+//  Created by huzhi on 2017/7/19.
+//  Copyright © 2017年 ZhiHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
